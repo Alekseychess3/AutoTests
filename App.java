bd630ab01ab25601dcbc063d54bc457465d89cdd
Alex <ochernega489@gmail.com>
2016-02-17 13:04:45 +0200

    version1

---
+++ App.java
@@ -0,0 +1,33 @@
+package org.apache.logging.log4j.log4j_api;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+/**
+ * Hello world!
+ *
+ */
+
+
+
+public class App
+{
+	//private static final Logger logger=LogManager.getLogger("HelloWorld");
+	private static final Logger logger = LogManager.getLogger("HelloWorld");
+    public static void main( String[] args )
+    {
+    	// TODO Auto-generated method stub
+    			System.out.println("привет группа!!");
+    			
+    			for (int m = 1; m <= 100; m++) {
+    				if (m%2==0)
+    				System.out.println(m); 
+    				logger.trace("step :: "+ m);
+    				logger.info("step :: "+ m);
+    				logger.warn("step :: "+ m);
+    				logger.error("step :: "+ m);
+    				
+    			}
+
+    }
+}
