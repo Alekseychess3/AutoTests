0198096f41bcaf9c817cb47a5ca1fde8d16d55a3
Alex <ochernega489@gmail.com>
2016-03-17 02:35:24 +0200

    v3

--- QA automation/tests/FeedBack/FeedBackTest.java
+++ QA automation/tests/FeedBack/FeedBackTest.java
@@ -3,7 +3,6 @@ import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
